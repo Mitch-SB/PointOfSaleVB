8bedfdb74211481563eb6a66f3b87c692d3a07a8
Mitch-SB <36740718+Mitch-SB@users.noreply.github.com>
2018-11-27 00:11:54 -0500

    Initial upload

---
+++ RegisterVB/Form1.Designer.vb
@@ -0,0 +1,219 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.groupBoxPad = New System.Windows.Forms.GroupBox()
+        Me.btnQty = New System.Windows.Forms.Button()
+        Me.btnEnter = New System.Windows.Forms.Button()
+        Me.btnDelete = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.groupBoxPad.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'groupBoxPad
+        '
+        Me.groupBoxPad.Controls.Add(Me.btnQty)
+        Me.groupBoxPad.Controls.Add(Me.btnEnter)
+        Me.groupBoxPad.Controls.Add(Me.btnDelete)
+        Me.groupBoxPad.Controls.Add(Me.btnClear)
+        Me.groupBoxPad.Controls.Add(Me.btn0)
+        Me.groupBoxPad.Controls.Add(Me.btn3)
+        Me.groupBoxPad.Controls.Add(Me.btn2)
+        Me.groupBoxPad.Controls.Add(Me.btn1)
+        Me.groupBoxPad.Controls.Add(Me.btn6)
+        Me.groupBoxPad.Controls.Add(Me.btn5)
+        Me.groupBoxPad.Controls.Add(Me.btn4)
+        Me.groupBoxPad.Controls.Add(Me.btn9)
+        Me.groupBoxPad.Controls.Add(Me.btn8)
+        Me.groupBoxPad.Controls.Add(Me.btn7)
+        Me.groupBoxPad.Location = New System.Drawing.Point(260, 196)
+        Me.groupBoxPad.Name = "groupBoxPad"
+        Me.groupBoxPad.Size = New System.Drawing.Size(281, 244)
+        Me.groupBoxPad.TabIndex = 16
+        Me.groupBoxPad.TabStop = False
+        '
+        'btnQty
+        '
+        Me.btnQty.Location = New System.Drawing.Point(190, 19)
+        Me.btnQty.Name = "btnQty"
+        Me.btnQty.Size = New System.Drawing.Size(85, 50)
+        Me.btnQty.TabIndex = 13
+        Me.btnQty.Text = "Qty"
+        Me.btnQty.UseVisualStyleBackColor = True
+        '
+        'btnEnter
+        '
+        Me.btnEnter.Location = New System.Drawing.Point(190, 187)
+        Me.btnEnter.Name = "btnEnter"
+        Me.btnEnter.Size = New System.Drawing.Size(85, 50)
+        Me.btnEnter.TabIndex = 12
+        Me.btnEnter.Text = "Enter"
+        Me.btnEnter.UseVisualStyleBackColor = True
+        '
+        'btnDelete
+        '
+        Me.btnDelete.Location = New System.Drawing.Point(190, 131)
+        Me.btnDelete.Name = "btnDelete"
+        Me.btnDelete.Size = New System.Drawing.Size(85, 50)
+        Me.btnDelete.TabIndex = 11
+        Me.btnDelete.Text = "<=="
+        Me.btnDelete.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(190, 75)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(85, 50)
+        Me.btnClear.TabIndex = 10
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btn0
+        '
+        Me.btn0.Location = New System.Drawing.Point(62, 187)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(50, 50)
+        Me.btn0.TabIndex = 9
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = True
+        '
+        'btn3
+        '
+        Me.btn3.Location = New System.Drawing.Point(118, 131)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(50, 50)
+        Me.btn3.TabIndex = 8
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        '
+        'btn2
+        '
+        Me.btn2.Location = New System.Drawing.Point(62, 131)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(50, 50)
+        Me.btn2.TabIndex = 7
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(6, 131)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(50, 50)
+        Me.btn1.TabIndex = 6
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'btn6
+        '
+        Me.btn6.Location = New System.Drawing.Point(118, 75)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(50, 50)
+        Me.btn6.TabIndex = 5
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        '
+        'btn5
+        '
+        Me.btn5.Location = New System.Drawing.Point(62, 75)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(50, 50)
+        Me.btn5.TabIndex = 4
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        '
+        'btn4
+        '
+        Me.btn4.Location = New System.Drawing.Point(6, 75)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(50, 50)
+        Me.btn4.TabIndex = 3
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        '
+        'btn9
+        '
+        Me.btn9.Location = New System.Drawing.Point(118, 19)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(50, 50)
+        Me.btn9.TabIndex = 2
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        '
+        'btn8
+        '
+        Me.btn8.Location = New System.Drawing.Point(62, 19)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(50, 50)
+        Me.btn8.TabIndex = 1
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        '
+        'btn7
+        '
+        Me.btn7.Location = New System.Drawing.Point(6, 19)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(50, 50)
+        Me.btn7.TabIndex = 0
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(553, 452)
+        Me.Controls.Add(Me.groupBoxPad)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.groupBoxPad.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Private WithEvents groupBoxPad As GroupBox
+    Private WithEvents btnQty As Button
+    Private WithEvents btnEnter As Button
+    Private WithEvents btnDelete As Button
+    Private WithEvents btnClear As Button
+    Private WithEvents btn0 As Button
+    Private WithEvents btn3 As Button
+    Private WithEvents btn2 As Button
+    Private WithEvents btn1 As Button
+    Private WithEvents btn6 As Button
+    Private WithEvents btn5 As Button
+    Private WithEvents btn4 As Button
+    Private WithEvents btn9 As Button
+    Private WithEvents btn8 As Button
+    Private WithEvents btn7 As Button
+End Class
